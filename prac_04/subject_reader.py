e6e9d5e7b6eab44bb4629be94ee5530e0f25f7ca
jd154840cc2511 <dylan.locke@my.jcu.edu.au>
2026-06-14 19:41:28 +1000

    Updated subject reader to display data more coherently

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -9,23 +9,31 @@ FILENAME = "subject_data.txt"
 def main():
     """Program to load and display subject data from file."""
     data = load_data(FILENAME)
-    print(data)
+    display_subject_details(data)
 
 
 def load_data(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(filename)
+    subject_list = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         # Make the number an integer as part of a new, poorly named, list
         data = [parts[0], parts[1], int(parts[2])]
-        print(data)  # See if that worked
-        print("----------")
+        # print(data)  # See if that worked
+        # print("----------")
+        subject_list.append(data)
     input_file.close()
+    return subject_list
+
+
+def display_subject_details(data):
+    for i in data:
+        print(f"{i[0]} is taught by {i[1]} and has {i[2]} students")
 
 
 main()
