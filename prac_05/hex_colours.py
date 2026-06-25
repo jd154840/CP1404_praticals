637c5bc5a294ab84aff3b4985b8013ecb7a6bab1
jd154840cc2511 <dylan.locke@my.jcu.edu.au>
2026-06-25 21:23:54 +1000

    added hex colours practical to prac 5

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,13 @@
+COLOR_TO_CODE = {"aqua": "#00ffff", "ash grey": "#b2beb5", "army green": "#4b5320"}
+MAX_COLOR_LENGTH = max(len(name) for name in list(COLOR_TO_CODE.keys()))    #unnecessary for this exercise
+
+for i, color in enumerate(COLOR_TO_CODE):
+    print(f"Option {i + 1}: {color}")
+
+color = input("Select color from options: ").lower()
+while color != "":
+    try:
+        print(f"{color.upper()} code: {COLOR_TO_CODE[color]}")
+    except KeyError:
+        print("Invalid Color")
+    color = input("Select color from options: ").lower()
