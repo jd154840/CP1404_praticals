70137456006ebb000e01cde953150f1eed280b26
jd154840cc2511 <dylan.locke@my.jcu.edu.au>
2026-07-14 23:26:54 +1000

    Add programming_language exercises to prac_06

---
+++ prac_06/programming_languages.py
@@ -0,0 +1,10 @@
+from prac_06.languages import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+programming_languages = [python, ruby, visual_basic]
+print("The dynamically typed languages are:")
+for language in programming_languages:
+    if language.is_dynamic():
+        print(language.name)
